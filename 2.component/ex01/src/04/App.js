678aacb155dcad532e365f5f0a4dbf9239c927bb
sso <sojeong7524@gmail.com>
2023-11-06 17:42:31 +0900

    component property validation

--- 2.component/ex01/src/04/App.js
+++ 2.component/ex01/src/04/App.js
@@ -5,13 +5,19 @@ function App() {
     return (
         <div id={'App'}>
             <MyComponent
-                props01={10}
-                props02={''}
+                props02={20}
                 props03={true}
                 props04={{no:1, name:'둘리'}}
-                props05={[]}
-                props06={() => {}}
-                />
+                props05={['javascript', 'react', 'es6']}
+                props06={() => '함수'}
+                props07={10}
+                props08={[true, false, false]}
+                props09={{
+                    no: 10,
+                    name: '마이콜',
+                    email: 'michol@gmail.com'
+                }}
+            />
         </div>
     );
 }
