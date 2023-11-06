4b86af6f14cda5ea63d8165904668e978ec674af
sso <sojeong7524@gmail.com>
2023-11-06 16:33:33 +0900

    component #2

--- 2.component/ex01/src/02/FoodList.js
+++ 2.component/ex01/src/02/FoodList.js
@@ -16,10 +16,12 @@ function FoodList({foods}) {
     return (
         <ul>
             {
-                foods.map(food => <FoodListItem
-                                        key={food.no}
-                                        name={food.name}
-                                        count={food.count}/>)
+                foods.map(function(food) {
+                    return <FoodListItem
+                                key={food.no}
+                                name={food.name}
+                                count={food.count} />;
+                })
             }
         </ul>
     );
