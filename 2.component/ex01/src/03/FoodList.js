174508c3bfd0105f41808519d109532a8d91af73
sso <sojeong7524@gmail.com>
2023-11-06 16:42:04 +0900

    component #3 클래스 컴포넌트

---
+++ 2.component/ex01/src/03/FoodList.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import FoodListItem from './FoodListItem';
+
+class FoodList extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <ul>
+                {
+                    this.props.foods.map(function(food) {
+                        return <FoodListItem
+                                    key={food.no}
+                                    name={food.name}
+                                    count={food.count} />;
+                    })
+                }
+            </ul>
+        );
+    }
+}
+
+export default FoodList;
