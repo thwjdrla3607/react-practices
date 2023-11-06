98b8b69859e53b8690496cf3337719d1e0a11a7f
sso <sojeong7524@gmail.com>
2023-11-06 11:50:58 +0900

    jsx

---
+++ 1.basics/ex08/src/06/Clock02.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Clock02(props) {
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+
+    return (
+        <div>
+            {('0'+(hours > 12 ? hours-12 : hours)).slice(-2)}
+            {':'}
+            {('0'+minutes).slice(-2)}
+            {':'}
+            {('0' + seconds).slice(-2)}
+            {' '}
+            {hours > 12 ? 'PM' : 'AM'}
+        </div>
+    );
+}
+
+export default Clock02;
