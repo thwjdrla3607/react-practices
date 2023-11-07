fd1cf1b4772707bbc61f3db980004ae30e66b62f
sso <sojeong7524@gmail.com>
2023-11-07 15:53:13 +0900

    kanabanboard #1

---
+++ kanbanboard/frontend/src/Kanbanboard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import data from './assets/json/data';
+
+function Kanbanboard(props) {
+
+    const cardsToDo = data.filter(card => card.status === 'ToDo');
+    console.log(cardsToDo);
+
+    return (
+        <div>
+            Kanbanboard
+        </div>
+
+
+    );
+}
+
+export default Kanbanboard;
